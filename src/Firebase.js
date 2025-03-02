89c52af5d872fa6e9488aebd2c0e48c92e2fbe35
letsmatch4900 <letsmatch4900@gmail.com>
2025-03-02 18:36:37 -0500

    Update Firebase.js
    add firestore to config

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
 import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,7 +20,9 @@ const config = {
 };
 
 // Initialize Firebase
-const app = initializeApp(config);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
+export { db, auth };
   
