e5f1a2097166da02df5e1f25acca7a541ab10e99
ChristianC999 <christiancortesb9@gmail.com>
2025-03-10 15:35:20 -0400

    Updated App.jsx
    Added the new dashboard and questions

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Register from "./components/Register";
 import Recovery from "./components/Recovery";
+import AddQuestion from "./components/AddQuestion";
+import ReviewQuestion from "./components/ReviewQuestion";
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/recovery" element={<Recovery />} />
-
+        <Route path="/add-question" element={<AddQuestion />} />
+        <Route path="/review-question" element={<ReviewQuestion />} />
       </Routes>
     </Router>
   );
